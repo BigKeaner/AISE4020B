99fdb41c6ee28e5edbb1ddc1b8e434a9de40fa17
issaa71 <93705826+issaa71@users.noreply.github.com>
2025-03-27 15:27:07 -0400

    Create room_config.py
    File containing coordinates in workspace for different rooms

---
+++ room_config.py
@@ -0,0 +1,33 @@
+# Room coordinates from our mapping
+ROOM_COORDINATES = {
+    "dining_room": {
+        "x": -0.242059,
+        "y": 1.086841,
+        "orientation": 0.697542,  # z value from orientation
+        "description": "Front left room"
+    },
+    "kitchen": {
+        "x": 0.880428,
+        "y": 0.999587,
+        "orientation": 0.707844,
+        "description": "Middle left room"
+    },
+    "bathroom": {
+        "x": 0.803891,
+        "y": -0.144288,
+        "orientation": -0.698438,
+        "description": "Middle right room"
+    },
+    "living_room": {
+        "x": -0.172471,
+        "y": -0.302494,
+        "orientation": -0.690783,
+        "description": "Front right room"
+    },
+    "bedroom": {
+        "x": 1.651717,
+        "y": 0.022928,
+        "orientation": 0.012207,
+        "description": "Far top room"
+    }
+}
